e478873a872efa98d24c7b95680c4a5404eeabc0
gergling <gregmichaeldavies@gmail.com>
2014-08-27 17:16:39 +0100

    Justified background image.

--- src/templates/img/404-airlock/404-airlock.js
+++ src/templates/img/404-airlock/404-airlock.js
@@ -1,20 +1,20 @@
 var radius = 50;
 this.width = (radius * 4);
 this.height = (radius * 3);
+var ratio = this.height / this.width;
 var cx = this.width/2, cy = this.height/2;
-var l = new Point(
-    cx - radius,
-    cy
-);
-var r = new Point(
-    cx + radius,
-    cy
-);
+var l = new Point(cx - radius, cy);
+var r = new Point(cx + radius, cy);
+
 var background = new Image();
 background.src = "https://c3e0976d5fa48a0ee2571be341af11486e64e7de.googledrive.com/host/0Bxf8n7VcUjWsdlc0QUpRWUEzTUk/black-hole.jpg";
 background.onload = function() {
     // Draw in background image
-    ctx.drawImage(background, 0, 0, scope.width, scope.width * background.height / background.width);
+    var backgroundRatio = background.height / background.width, 
+        scale = ratio / backgroundRatio,
+        bgWidth = scope.width * scale,
+        xOffSet = (scope.width - bgWidth) * (scale / 2);
+    ctx.drawImage(background, xOffSet, 0, bgWidth, scope.height);
 
     ctx.beginPath();
     ctx.arc(cx, cy, radius, 0, 2 * Math.PI, false);

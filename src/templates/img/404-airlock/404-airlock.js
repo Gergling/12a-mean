6caccb925993bfa124baaf3aa8a27f2add8601ba
gergling <gregmichaeldavies@gmail.com>
2014-08-28 21:33:17 +0100

    Included shine.

--- src/templates/img/404-airlock/404-airlock.js
+++ src/templates/img/404-airlock/404-airlock.js
@@ -1,4 +1,9 @@
-var radius = 50;
+var radius = 50, 
+    rd2 = radius / 2,
+    rd4 = radius / 4,
+    rd8 = radius / 8,
+    rd16 = radius / 16,
+    rd32 = radius / 32;
 this.width = (radius * 4);
 this.height = (radius * 3);
 var ratio = this.height / this.width;
@@ -42,5 +47,56 @@ background.onload = function() {
     ctx.strokeStyle = 'darkred';
     ctx.stroke();
 
+    var grd=ctx.createLinearGradient(0,cy-radius,0,cy+radius);
+    grd.addColorStop(0,"#400");
+    grd.addColorStop(1,"#d00");
+    ctx.beginPath();
+    ctx.arc(cx, cy, radius - rd16, 0, 2 * Math.PI, false);
+    ctx.lineWidth = rd8;
+    ctx.strokeStyle = grd;
+    ctx.stroke();
+
+    grd=ctx.createLinearGradient(cx+(radius/2),cy-radius,cx-(radius/2),cy+radius);
+    grd.addColorStop(0,"#ff00ff");
+    grd.addColorStop(0.4,"orange");
+    grd.addColorStop(0.5,"red");
+    grd.addColorStop(0.85,"#c55");
+    grd.addColorStop(1,"#f0f");
+    ctx.beginPath();
+    ctx.arc(cx, cy, radius - (radius/8), 0, 2 * Math.PI, false);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = grd;
+    ctx.stroke();
+
+    ctx.lineWidth = radius/4;
+    ctx.strokeStyle = 'darkred';
     scope.drawLine(ctx, l, r);
+
+    grd = ctx.createLinearGradient(cx-radius,0,cx+radius,0);
+    grd.addColorStop(0,"red");
+    grd.addColorStop(0.4,"orange");
+    grd.addColorStop(0.48,"white");
+    grd.addColorStop(0.55,"#fdf");
+    grd.addColorStop(0.7,"#f0f");
+    grd.addColorStop(1,"darkred");
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = grd;
+    var al = new Point();al.copy(l);
+    var ar = new Point();ar.copy(r);
+    al.x += rd8;
+    al.y -= rd8;ar.y = al.y;
+    ar.x -= rd8;
+    scope.drawLine(ctx, al, ar);
+
+    grd = ctx.createLinearGradient(cx-radius,0,cx+radius,0);
+    grd.addColorStop(0,"darkred");
+    grd.addColorStop(0.1,"red");
+    grd.addColorStop(0.4,"orange");
+    grd.addColorStop(0.52,"white");
+    grd.addColorStop(0.6,"orange");
+    grd.addColorStop(0.9,"red");
+    grd.addColorStop(1,"darkred");
+    ctx.strokeStyle = grd;
+    al.y += rd4;ar.y = al.y;
+    scope.drawLine(ctx, al, ar);
 };

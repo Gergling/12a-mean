0e43d7941da482c12f540938c3a80cc5ab7d79b9
gergling <gregmichaeldavies@gmail.com>
2014-08-28 12:47:46 +0100

    Included grey background.

--- src/templates/img/404-airlock/404-airlock.js
+++ src/templates/img/404-airlock/404-airlock.js
@@ -13,8 +13,28 @@ background.onload = function() {
     var backgroundRatio = background.height / background.width, 
         scale = ratio / backgroundRatio,
         bgWidth = scope.width * scale,
-        xOffSet = (scope.width - bgWidth) * (scale / 2);
+        xOffSet = (scope.width - bgWidth) * (scale / 2),
+        i;
     ctx.drawImage(background, xOffSet, 0, bgWidth, scope.height);
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'grey';
+    var drawHLine = function (y) {
+        var al = new Point(0, y);
+        var ar = new Point(scope.width, y);
+        scope.drawLine(ctx, al, ar);
+    };
+    for(i = 0; i < cy - radius; i++) {drawHLine(i);}
+    for(i = cy - radius; i < cy + radius; i++) {
+        var xOffset = Math.sqrt(Math.pow(radius, 2) - Math.pow(i - cy, 2));
+        var ll = new Point(0, i);
+        var lr = new Point(cx - xOffset, i);
+        scope.drawLine(ctx, ll, lr);
+        var rl = new Point(cx + xOffset, i);
+        var rr = new Point(scope.width, i);
+        scope.drawLine(ctx, rl, rr);
+    }
+    for(i = cy + radius; i < scope.height; i++) {drawHLine(i);}
 
     ctx.beginPath();
     ctx.arc(cx, cy, radius, 0, 2 * Math.PI, false);

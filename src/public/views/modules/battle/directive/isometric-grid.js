175d7fb09ab725cd414bd04f13fd98563ab26fab
Greg <gregmichaeldavies@gmail.com>
2014-09-25 23:11:57 +0100

    Linted.

--- src/public/views/modules/battle/directive/isometric-grid.js
+++ src/public/views/modules/battle/directive/isometric-grid.js
@@ -11,11 +11,11 @@ ngModules.get("battle").component(function (ngm, mod) {
 
                 function ($scope) {
                     var x, y, row, cell;
-                    $scope.cellHeight = $scope.cellWidth*Math.sin(Math.PI*30/360)/Math.sin(Math.PI*60/360);
+                    $scope.cellHeight = $scope.cellWidth * Math.sin(Math.PI * 30 / 360) / Math.sin(Math.PI * 60 / 360);
                     $scope.grid = [];
-                    for(y = 0; y < 3; y += 1) {
+                    for (y = 0; y < 3; y += 1) {
                         row = [];
-                        for(x = 0; x < 3; x += 1) {
+                        for (x = 0; x < 3; x += 1) {
                             cell = {x: x, y: y};
                             cell.isoX = x + y;
                             cell.isoY = x + 1 - y;

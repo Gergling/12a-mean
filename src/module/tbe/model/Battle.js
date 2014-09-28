6745650fe0daf536b23ed551cd7910f09ade0a06
Greg <gregmichaeldavies@gmail.com>
2014-09-28 14:25:43 +0100

    Fleshing out Battle class.

--- src/module/tbe/model/Battle.js
+++ src/module/tbe/model/Battle.js
@@ -1,9 +1,42 @@
-module.exports = function () {
+module.exports = function (tbeSchemas) {
+    var BattleModel = tbeSchemas.Battle.model;
     return function () {
-        this.save = function () {
+        var scope = this,
+            battleFactory = "(unknown-battle-factory)",
+            participants = [ ];
+        this.save = function (success, error) {
+            error = error || function (err) {
+                console.error("tbe.model.Battle - error with no callback:", err.message, err.error);
+            };
+
             // Get the Battle model
+            Battle.findOne({}, function (err, model) {
+                if (!model) {
+                    model = new BattleModel({
+                        battle_factory: scope.factory,
+                        participants: participants
+                    });
+                }
+                model.save(function (err) {
+                    if (err) {
+                        error({
+                            message: "Error creating battle",
+                            error: err
+                        });
+                    } else {
+                        success(battle);
+                    }
+                });
+            });
+
             // Set it up in line with this battle.
             // Save it.
+        };
+        this.set = function (model) {
+            // Sets Battle properties to be what the model has.
+            // This will include all characters involved and battle factory name.
+            battleFactory = model.battle_factory;
+            //scope.
         };
     };
 };

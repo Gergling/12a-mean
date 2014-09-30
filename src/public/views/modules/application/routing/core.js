3f142906d4858c795f82c7374704bd3ad2b2e325
Greg <gregmichaeldavies@gmail.com>
2014-09-30 23:45:18 +0100

    Implemented primary navigation.

--- src/public/views/modules/application/routing/core.js
+++ src/public/views/modules/application/routing/core.js
@@ -5,9 +5,10 @@ ngModules.get("application").component(function (ngm, mod) {
         var getPartialUrl = function (name) {return 'modules/' + mod.getPartialUrl(name); };
 
         var routes = {
-            '/': {partial: getPartialUrl('index')},
-            '/test/': {partial: 'modules/test/partial/test.html'},
-            '/battle/': {partial: 'modules/battle/partial/battle.html'}
+            '/': {redirectTo: "/mess/"},
+            '/mess/': {partial: getPartialUrl('index'), name: "mess"},
+            '/cargo-bay/': {partial: 'modules/test/partial/test.html', name: "cargo-bay"},
+            '/bridge/': {partial: 'modules/battle/partial/battle.html', name: "bridge"}
         };
         angular.forEach(routes, function (obj, route) {
             obj.templateUrl = getPartialUrl('container');
@@ -15,9 +16,17 @@ ngModules.get("application").component(function (ngm, mod) {
         });
         $routeProvider.otherwise({templateUrl: getPartialUrl('container'), partial: getPartialUrl('404')});
     }]);
-    ngm.controller(mod.getModuleName("controller", "index"), ["$rootScope", function ($scope) {
-        $scope.$on("$routeChangeStart", function (event, next, current) {
-            $scope.routeTemplateUrl = next.partial;
-        });
-    }]);
+    ngm.controller(mod.getModuleName("controller", "index"), [
+
+        "$rootScope",
+        "application.service.primary-navigation",
+
+        function ($scope, navigation) {
+            $scope.navigation = navigation;
+            $scope.$on("$routeChangeStart", function (event, next, current) {
+                $scope.routeTemplateUrl = next.partial;
+                navigation.setActive(next.name);
+            });
+        }
+    ]);
 });

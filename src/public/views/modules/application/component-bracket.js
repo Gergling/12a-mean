2d89e3fffba3caf46368f12d327a7aff2a27ed66
Greg <gregmichaeldavies@gmail.com>
2014-09-30 21:44:35 +0100

    Component bracket for overall layout.

--- src/public/views/modules/application/component-bracket.js
+++
@@ -1,9 +0,0 @@
-ngModules.get("battle").component(function (ngm, mod) {
-    "use strict";
-
-    ngm.directive("component-bracket", function () {
-        return {
-            templateUrl: 'modules/' + mod.getPartialUrl("component-bracket")
-        };
-    });
-});

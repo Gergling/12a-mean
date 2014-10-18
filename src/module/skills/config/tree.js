d28c786f8d2b9afc47e9a4e105de198673a80ecc
Greg <gregmichaeldavies@gmail.com>
2014-10-19 00:46:54 +0100

    Updated skill tree configuration.

--- src/module/skills/config/tree.js
+++ src/module/skills/config/tree.js
@@ -7,42 +7,41 @@
 // This will require a script to scan the database for these anomalies.
 
 module.exports = function () {
-    var SkillCollection = require("../model/SkillCollection")(),
-        collection = new SkillCollection();
+    var SkillNode = require("../model/SkillNode")(),
+        root = new Skillnode();
 
-    //collection.set(tree);
-    collection.setNode("combat").set({description: "Skills in murder when met with resistance"});
-    collection.setNode("combat.strategy").set({description: "Understanding of applied knowledge in combat"});
-    collection.setNode("combat.strategy.weapons");
-    collection.setNode("combat.strategy.obfuscation");
-    collection.setNode("combat.practical").set({description: "Instinctive application of combat skills"});
-    collection.setNode("combat.practical.unarmed").set({description: "The ability to fight without holding weapons"});
-    collection.setNode("combat.practical.weapons").set({description: "The ability to fight using weapons"});
-    collection.setNode("combat.practical.weapons.mounted").set({description: "Weapons mounted on a vehicle or turret"});
-    collection.setNode("combat.practical.weapons.personnel").set({description: "Weapons carried by sentient beings"});
-    collection.setNode("science").set({description: "Understanding of scientific methodology and its products"});
-    collection.setNode("science.model");
-    collection.setNode("science.model.biology");
-    collection.setNode("science.model.biology.xeno");
-    collection.setNode("science.model.biology.terran");
-    collection.setNode("science.model.biology.terran.animal");
-    collection.setNode("science.model.biology.terran.animal.mammal");
-    collection.setNode("science.model.biology.terran.animal.mammal.human");
-    collection.setNode("science.model.physics");
-    collection.setNode("science.model.psychology");
-    collection.setNode("science.technology");
-    collection.setNode("science.technology.medical");
-    collection.setNode("science.technology.engineering");
-    collection.setNode("science.technology.engineering.weapons");
-    collection.setNode("science.technology.engineering.engines");
-    collection.setNode("science.technology.engineering.defenses");
-    collection.setNode("science.technology.engineering.scanners");
-    collection.setNode("science.technology.engineering.environmental");
-    collection.setNode("science.technology.engineering.personnel");
-    collection.setNode("science.technology.engineering.encryption");
-    collection.setNode("charisma").set({description: "An understanding of how to communicate with other sentient beings"});
-    collection.setNode("lore").set({description: "General Knowledge"});
-    collection.setNode("lore.religion");
+    root.set("combat").set({description: "Skills in murder when met with resistance"});
+    root.set("combat.strategy").set({description: "Understanding of applied knowledge in combat"});
+    root.set("combat.strategy.weapons");
+    root.set("combat.strategy.obfuscation");
+    root.set("combat.practical").set({description: "Instinctive application of combat skills"});
+    root.set("combat.practical.unarmed").set({description: "The ability to fight without holding weapons"});
+    root.set("combat.practical.weapons").set({description: "The ability to fight using weapons"});
+    root.set("combat.practical.weapons.mounted").set({description: "Weapons mounted on a vehicle or turret"});
+    root.set("combat.practical.weapons.personnel").set({description: "Weapons carried by sentient beings"});
+    root.set("science").set({description: "Understanding of scientific methodology and its products"});
+    root.set("science.model");
+    root.set("science.model.biology");
+    root.set("science.model.biology.xeno");
+    root.set("science.model.biology.terran");
+    root.set("science.model.biology.terran.animal");
+    root.set("science.model.biology.terran.animal.mammal");
+    root.set("science.model.biology.terran.animal.mammal.human");
+    root.set("science.model.physics");
+    root.set("science.model.psychology");
+    root.set("science.technology");
+    root.set("science.technology.medical");
+    root.set("science.technology.engineering");
+    root.set("science.technology.engineering.weapons");
+    root.set("science.technology.engineering.engines");
+    root.set("science.technology.engineering.defenses");
+    root.set("science.technology.engineering.scanners");
+    root.set("science.technology.engineering.environmental");
+    root.set("science.technology.engineering.personnel");
+    root.set("science.technology.engineering.encryption");
+    root.set("charisma").set({description: "An understanding of how to communicate with other sentient beings"});
+    root.set("lore").set({description: "General Knowledge"});
+    root.set("lore.religion");
 
-    return collection;
+    return root;
 };

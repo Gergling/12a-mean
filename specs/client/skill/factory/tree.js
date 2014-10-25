814fcb6f3c59e229f5317bfbd55a0e81c4fab469
Greg <gregmichaeldavies@gmail.com>
2014-10-25 12:52:18 +0100

    FE tree behaviour.

---
+++ specs/client/skill/factory/tree.js
@@ -0,0 +1,33 @@
+describe("Skill Tree Factory (skill.factory.tree)", function () {
+    var factory, $httpBackend;
+
+    beforeEach(function () {
+        module('application');
+        module('skill');
+        inject(function ($injector) {
+            $httpBackend = $injector.get("$httpBackend");
+            $httpBackend.expectGET("/skills").respond({
+                level: 0,
+                children: {
+                    combat: {
+                        name: "combat",
+                        level: 1,
+                        children: {
+                            strategy: {
+                                name: "strategy",
+                                level: 2
+                            }
+                        }
+                    }
+                }
+            });
+            factory = $injector.get("skill.service.tree");
+            $httpBackend.flush();
+        });
+    });
+
+    it("on instantiation, loads a hierarchy of skill nodes from the backend", function () {
+        expect(factory.get(["combat"]).level).toBe(1);
+        expect(factory.get(["combat", "strategy"]).level).toBe(2);
+    });
+});

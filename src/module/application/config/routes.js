7bd56f6bce97a7f58470620ee22874383e6913ca
Greg <gregmichaeldavies@gmail.com>
2014-10-28 22:00:20 +0000

    Basic PUT configuration.

--- src/module/application/config/routes.js
+++ src/module/application/config/routes.js
@@ -47,7 +47,37 @@ module.exports = function (app, tbeSchemas, controllers, mongoose) {
             res.status(403).end();
         }
     });
+
     // PUT /battle casts an ability
+    app.param('abilityName', /^[a-z]$/);
+    app.put('/battle/:abilityName', function (req, res) {
+        var player_id = 1;
+        if (player_id) {
+            controllers.battle.ability(
+                player_id,
+                req.params.abilityName,
+                req.query // Controller to sanitise.
+            ).then(function () {
+                res.send({ });
+            }, function (error) {
+                switch (error.type) {
+                case "malformed":
+                    res.status(400).send(error.payload);
+                    break;
+                case "unavailable":
+                    res.status(403).end();
+                    break;
+                case "nonexistent":
+                    res.status(404).end();
+                    break;
+                default:
+                    res.status(500).end();
+                }
+            });
+        } else {
+            res.status(401).end();
+        }
+    });
 
     // POST /battle starts a new battle.
     app.post('/battle', function (req, res) {

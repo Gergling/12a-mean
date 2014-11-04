4704daa2434bbf3a76d6857e099950fcab906ec2
Greg <gregmichaeldavies@gmail.com>
2014-11-04 22:06:05 +0000

    Main generator work.

--- src/templates/image-generator/generator.js
+++ src/templates/image-generator/generator.js
@@ -3,20 +3,33 @@ module.exports = (function () {
 
     var grunt = require("grunt"),
         pathService = require("path"),
+        fs = require('fs'),
+        PNG = require('pngjs').PNG,
 
+        newPNG = function () {
+            var png = new PNG();
+            if (!png.data) {png.data = [ ]; }
+            png.width = 1;
+            png.height = 1;
+            png.alpha = true;
+            return png;
+        },
         paths = {
+            module: "./",
             generator: "src/templates/image-generator/"
         };
 
     grunt.file.expand(paths.generator + "src/*.js").forEach(function (srcPath) {
         var fileName = pathService.basename(srcPath, ".js"),
-            distribution = paths.generator + "dist/" + fileName + ".png";
+            distPath = paths.generator + "dist/" + fileName + ".png",
+            dst = fs.createWriteStream(distPath),
+            png = newPNG();
 
-        console.log(distribution);
-        //contexts[contextName] = require(modulePath)(context);
-        // Inject png instance
-        //require(srcPath);
-        // Expect png object returned
+        require(paths.module + "src/" + fileName)(png);
+
+        grunt.log.writeln("- Image generated: '" + fileName + "'");
+
+        png.pack().pipe(dst);
     });
 
 }());

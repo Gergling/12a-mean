25d97c7ee1aec5f759a0ad2d8138a9c10b4c6a86
gergling <gregmichaeldavies@gmail.com>
2014-11-07 07:48:14 +0000

    Removed ngModules reliance.

--- src/public/views/modules/application/factory/primary-navigation.js
+++ src/public/views/modules/application/factory/primary-navigation.js
@@ -1,30 +1,28 @@
-ngModules.get("application").component(function (ngm, mod) {
+angular.module("application").service("application.service.primary-navigation", function () {
     "use strict";
 
-    ngm.service(mod.getModuleName("service", "primary-navigation"), function () {
-        var scope = this, setNavItem = function (name, label) {
-            var item = {
-                label: label,
-                name: name
-            };
-            scope.list.push(item);
+    var scope = this, setNavItem = function (name, label) {
+        var item = {
+            label: label,
+            name: name
         };
-        this.list = [];
+        scope.list.push(item);
+    };
+    this.list = [];
 
-        setNavItem("mess", "Mess");
-        setNavItem("bridge", "Bridge");
-        setNavItem("cargo-bay", "Cargo Bay");
-        setNavItem("skills", "Skills");
+    setNavItem("mess", "Mess");
+    setNavItem("bridge", "Bridge");
+    setNavItem("cargo-bay", "Cargo Bay");
+    setNavItem("skills", "Skills");
 
-        this.setActive = function (name) {
-            angular.forEach(scope.list, function (item) {
-                if (item.name === name) {
-                    item.active = true;
-                    scope.active = item;
-                } else {
-                    item.active = false;
-                }
-            });
-        };
-    });
+    this.setActive = function (name) {
+        angular.forEach(scope.list, function (item) {
+            if (item.name === name) {
+                item.active = true;
+                scope.active = item;
+            } else {
+                item.active = false;
+            }
+        });
+    };
 });

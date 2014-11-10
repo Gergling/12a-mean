fbb690179eb620b188d22bbda4753b358e9231a7
gergling <gregmichaeldavies@gmail.com>
2014-11-10 08:28:05 +0000

    Interpolation fix.

--- src/templates/image-generator/generator/maths.js
+++ src/templates/image-generator/generator/maths.js
@@ -3,7 +3,7 @@ module.exports = (function () {
 
     return {
         interpolate: function (min, max, from, to, current) {
-            return (current * (max - min) / (to - from)) + min;
+            return ((current - from) * (max - min) / (to - from)) + min;
         },
         pythagoras: function (x, y) {
             return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));

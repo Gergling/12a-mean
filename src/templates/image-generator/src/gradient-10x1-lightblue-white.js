4ab9029e54a0a01244f1356152eecb715cd3698f
gergling <gregmichaeldavies@gmail.com>
2014-11-10 08:29:03 +0000

    Included rudimentary opposing gradient.

--- src/templates/image-generator/src/gradient-10x1-lightblue-white.js
+++ src/templates/image-generator/src/gradient-10x1-lightblue-white.js
@@ -6,13 +6,23 @@ module.exports = function (png, generator) {
             return parseInt(hex, 16);
         };
 
-    png.width = 10;
+    png.width = 19;
 
     generator.raster.gradient(new Pixel(
         getDec("20"),
         getDec("d0"),
         getDec("f0")
-    ), new Pixel(255, 255, 255));
+    ), new Pixel(255, 255, 255),
+        0, 0,
+        10, 0);
+
+    generator.raster.gradient(new Pixel(255, 255, 255), new Pixel(
+        getDec("20"),
+        getDec("d0"),
+        getDec("f0")
+    ),
+        10, 0,
+        19, 0);
 
     return png;
 };

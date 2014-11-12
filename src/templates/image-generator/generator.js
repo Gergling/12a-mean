d7fa70d8d280eca067a0bf630df32a5ddebe72cd
gergling <gregmichaeldavies@gmail.com>
2014-11-12 21:21:47 +0000

    Included failing code block.

--- src/templates/image-generator/generator.js
+++ src/templates/image-generator/generator.js
@@ -21,18 +21,42 @@ module.exports = (function () {
             generator: "src/templates/image-generator/"
         };
 
+    var x = new PNG();
+    x.width = 2;
+    x.height = 1;
+    x.data = [ ];
+    var channel;
+    channel = 0 << 2;
+    x.data[channel] = 0;
+    x.data[channel + 1] = 0;
+    x.data[channel + 2] = 0;
+    x.data[channel + 3] = 255;
+    channel = 1 << 2;
+    x.data[channel] = 0;
+    x.data[channel + 1] = 0;
+    x.data[channel + 2] = 0;
+    x.data[channel + 3] = 255;
+    channel = 2 << 2;
+    x.data[channel] = 0;
+    x.data[channel + 1] = 0;
+    x.data[channel + 2] = 0;
+    x.data[channel + 3] = 255;
+    console.log("Just before pack", x);
+    x.pack();
+
     grunt.file.expand(paths.generator + "src/*.js").forEach(function (srcPath) {
         var fileName = pathService.basename(srcPath, ".js"),
             distPath = paths.generator + "dist/" + fileName + ".png",
             dst = fs.createWriteStream(distPath),
             png = newPNG(),
-            fnc = require(paths.module + "src/" + fileName);
+            fnc = require(paths.module + "src/" + fileName),
+            gen = new Generator(png);
 
         if (typeof fnc === "function") {
-            fnc(png, new Generator(png));
-            grunt.log.writeln("- Image generated: '" + fileName + "'");
+            fnc(png, gen);
 
             png.pack().pipe(dst); // Todo: Touch dist folder.
+            grunt.log.writeln("- Image generated: '" + fileName + "'");
             // Also, it would be nice to know why the laptop doesn't want to let me 
             // use the function keys.
         } else {

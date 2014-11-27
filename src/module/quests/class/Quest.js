2af489530bc0c9f45186074997b08be384e5a324
gergling <gregmichaeldavies@gmail.com>
2014-11-27 22:47:09 +0000

    Quest generation.

--- src/module/quests/class/Quest.js
+++ src/module/quests/class/Quest.js
@@ -1,8 +1,29 @@
 module.exports = function () {
-    var extend = require("deep-extend");
+    "use strict";
 
-    return function (props) {
-        var scope = this;
-        this.rewards;
+    var Battle = require("../tbe/model/Battle"),
+        MapFactorySquare = require("../tbe/factory/MapFactorySquare"),
+        battle;
+
+    // Quest has a battle attached to it.
+    this.battle = function (value) {
+        if (value) {battle = value; }
+        return battle;
+    };
+
+    // Quest needs to evaluate the status of the battle for completion.
+    // When complete, the player is rewarded.
+    this.generateBattle = function () {
+        // Temporary function to generate a battle.
+        var mapFactory = new MapFactorySquare();
+
+        this.battle(new Battle());
+        this.battle().map(mapFactory.generate());
+    };
+
+    this.view = function () {
+        return {
+            battle: this.battle().view()
+        };
     };
 };

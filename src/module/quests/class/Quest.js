1fca38499513109ac30898c2af4063d0d8b05cfe
gergling <gregmichaeldavies@gmail.com>
2014-12-03 21:24:06 +0000

    Controller returns list of quests.

--- src/module/quests/class/Quest.js
+++ src/module/quests/class/Quest.js
@@ -1,8 +1,13 @@
 module.exports = function () {
     "use strict";
 
-    var Battle = require("../tbe/model/Battle"),
-        MapFactorySquare = require("../tbe/factory/MapFactorySquare"),
+    var Battle = require("../../tbe/model/Battle"),
+        mapFactory = require("../../tbe/factory/MapFactorySquare"),
+
+        context = "no-context",
+        type = "no-type",
+        label = "Mystery Quest!",
+        description = "This quest is a mystery.",
         battle;
 
     // Quest has a battle attached to it.
@@ -10,20 +15,44 @@ module.exports = function () {
         if (value) {battle = value; }
         return battle;
     };
+    this.context = function (value) {
+        if (value) {context = value; }
+        return context;
+    };
+    this.type = function (value) {
+        if (value) {type = value; }
+        return type;
+    };
+    this.label = function (value) {
+        if (value) {label = value; }
+        return label;
+    };
+    this.description = function (value) {
+        if (value) {description = value; }
+        return description;
+    };
 
     // Quest needs to evaluate the status of the battle for completion.
     // When complete, the player is rewarded.
     this.generateBattle = function () {
         // Temporary function to generate a battle.
-        var mapFactory = new MapFactorySquare();
-
-        this.battle(new Battle());
-        this.battle().map(mapFactory.generate());
+        this.battle(new Battle())
+            .map(mapFactory.generate());
     };
 
     this.view = function () {
         return {
-            battle: this.battle().view()
+            //battle: this.battle().view()
+            context: context,
+            type: type,
+            label: label,
+            description: description
+            // Needs an id. This will be an id for each 
+                // available quest for that player.
         };
+    };
+
+    this.startBattle = function () {
+        return true;
     };
 };
--- src/module/quests/controller.js
+++ src/module/quests/controller.js
@@ -4,14 +4,57 @@ module.exports = (function () {
     var Quest = require("./class/Quest"),
         controller = { };
 
-    controller.get = function () {
+    controller.list = function () {
         // Return a list of quests that the player is eligible for.
         // List would come from database.
         /// In the meantime, we're going to pretend.
-        var quest = new Quest();
+        var quests = [ ];
+        [
+            {context: "space-intel", type: "emergency",
+                label: "Scan Approaching Object",
+                description: "Something is matching our course and speed. "
+                    + "Find out what it is."},
+            {context: "space-intel", type: "buff",
+                label: "Navigation Feed",
+                description: "Offer the navigator an information feed, "
+                    + "such that movement space is increased overall."},
+            {context: "space-intel", type: "buff",
+                label: "Tactical Feed",
+                description: "Offer the tactical chief an information "
+                    + "feed, such that weapon and defense usage is "
+                    + "improved."},
+            {context: "space-intel", type: "buff",
+                label: "Environmental Feed",
+                description: "Setup a general feed of the external "
+                    + "environment, such that ship systems can be "
+                    + "optimised more efficiently."},
+            {context: "space-intel", type: "practise",
+                label: "General Astronomy",
+                description: "Look out into the stars and and uncover the "
+                    + "mysteries of space. There is the potential for "
+                    + "valuable data out there."},
+            {context: "space-intel", type: "mission",
+                label: "Scan Mystery Void",
+                description: "There is an unusually empty area of space "
+                    + "at {{x}}. {{y}} has asked that you find out what "
+                    + "resides there."}
+        ].forEach(function (obj) {
+            var quest = new Quest(),
+                view;
+            quest.context(obj.context);
+            quest.type(obj.type);
+            quest.label(obj.label);
+            quest.description(obj.description);
+            view = quest.view();
+            view.idx = quests.length;
+            quests.push(view);
+        });
+        return quests;
+    };
 
-        quest.generateBattle();
-        return [ quest ];
+    controller.startMission = function (questId) {
+        return { success: false, message: "I would like to tell you the "
+            + "message updated successfully. I'd like to tell you that..." };
     };
 
     return controller;

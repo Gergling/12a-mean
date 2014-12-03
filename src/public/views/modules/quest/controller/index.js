0bea143d8e26843e55cf0dcb22cf5197f0da5720
gergling <gregmichaeldavies@gmail.com>
2014-12-03 21:25:59 +0000

    Adjusted FE to receive quest format.

--- src/public/views/modules/quest/controller/index.js
+++ src/public/views/modules/quest/controller/index.js
@@ -4,23 +4,15 @@ ngModules.get("quest").component(function (ngm, mod) {
     ngm.controller(mod.getModuleName("controller", "index"), [
 
         "$scope",
+        "$location",
+        "quest.service.api",
 
-        function ($scope) {
-            $scope.quests = [
-                {context: "space-intel", type: "emergency", label: "Scan Approaching Object", description: "Something is matching our course and speed. Find out what it is."},
-                {context: "space-intel", type: "buff", label: "Navigation Feed", description: "Offer the navigator an information feed, such that movement space is increased overall."},
-                {context: "space-intel", type: "buff", label: "Tactical Feed", description: "Offer the tactical chief an information feed, such that weapon and defense usage is improved."},
-                {context: "space-intel", type: "buff", label: "Environmental Feed", description: "Setup a general feed of the external environment, such that ship systems can be optimised more efficiently."},
-                {context: "space-intel", type: "practise", label: "General Astronomy", description: "Look out into the stars and and uncover the mysteries of space. There is the potential for valuable data out there."},
-                {context: "space-intel", type: "mission", label: "Scan Mystery Void", description: "There is an unusually empty area of space at {{x}}. {{y}} has asked that you find out what resides there."}
-            ];
-            $scope.selected = {};
+        function ($scope, $location, api) {
+            $scope.quests = [ ];
+            $scope.selected = { };
             $scope.select = function (idx) {
                 $scope.selected = $scope.quests[idx];
             };
-            angular.forEach($scope.quests, function (quest, idx) {
-                quest.idx = idx;
-            });
 
             // Role colours: (for buff icon backgrounds)
             // - Navigator: blue
@@ -28,6 +20,30 @@ ngModules.get("quest").component(function (ngm, mod) {
             // - Intel: light blue
             // - Medic: green
             // - Tactical: red
+
+            api.fetch().then(function (list) {
+                $scope.quests = list;
+            });
+
+            $scope.awaitingStartMission = false;
+            $scope.successfulStartMission = false;
+            $scope.status = {
+                startMission: {
+                    awaiting: false,
+                    successful: false
+                }
+            };
+            $scope.startMission = function (idx) {
+                // Send a PUT to quests with the idx for quest acceptance.
+                // When successful, redirect to #/bridge/battle/
+                $scope.status.startMission.awaiting = true;
+                api.startMission(idx).then(function () {
+                    $scope.status.startMission.successful = true;
+                    ///$location.href();
+                }).finally(function () {
+                    $scope.status.startMission.awaiting = false;
+                });
+            };
         }
     ]);
 });

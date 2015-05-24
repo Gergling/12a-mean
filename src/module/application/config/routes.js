1eba99fe927477ab12200b68cbddf65ac9b6a656
gergling <gregmichaeldavies@gmail.com>
2015-05-24 15:01:03 +0100

    Removed superfluous comments.

--- src/module/application/config/routes.js
+++ src/module/application/config/routes.js
@@ -57,98 +57,6 @@ module.exports = function (app) {
         ]
     ]);
 
-    // GET /battle returns the state of the current battle
-    /*app.get('/battle', function (req, res) {
-        var player_id = 1;
-        if (player_id) {
-            controllers.battle.get(
-                player_id,
-                function (battle) {
-                    res.send(battle);
-                },
-                function (err) {
-                    res.send(err);
-                    //res.status(500).end();
-                }
-            );
-        } else {
-            res.send();
-            res.status(403).end();
-        }
-    });*/
-    // POST /battle/start/:mission-id
-    // - Starts a new battle based on the mission
-
-    // POST /battle/cast/:ability-id
-    // - Uses an ability
-    // - Body contains various ability details
-
-    // POST /battle/turn
-    // - Turns the game
-
-    // PUT /battle casts an ability
-    /*app.param('abilityName', function (req, res, next, name) {
-        var regex = new RegExp(/^[a-z]$/);
-        if (regex.test(name)) {
-            next();
-        } else {
-            next('route');
-        }
-    });
-    app.put('/battle/:abilityName', function (req, res) {
-        var player_id = 1;
-        if (player_id) {
-            controllers.battle.ability(
-                player_id,
-                req.params.abilityName,
-                req.query // Controller to sanitise.
-            ).then(function () {
-                res.send({ });
-            }, function (error) {
-                switch (error.type) {
-                case "malformed":
-                    res.status(400).send(error.query);
-                    break;
-                case "unavailable":
-                    res.status(403).end();
-                    break;
-                case "nonexistent":
-                    res.status(404).end();
-                    break;
-                default:
-                    res.status(500).end();
-                }
-            });
-        } else {
-            res.status(401).end();
-        }
-    });*/
-
-    // POST /battle starts a new battle.
-    /*app.post('/battle', function (req, res) {
-        // Will need to check for authentication.
-        // If authentication successful, populate player_id
-        var player_id = 1;
-        if (player_id) {
-            controllers.battle.start(
-                "space-intel", // context
-                req.param("battle_factory"),
-                player_id,
-                function (battle) {
-                    // Catch error and send as appropriate error message.
-                    res.send(battle);
-                },
-                function (err) {
-                    res.send(err);
-                    //res.status(401).end();
-                }
-            );
-        } else {
-            res.send();
-            res.status(403).end();
-        }
-    });*/
-
     routes('../../skill/controller', [
         [ 'get',    '/skill', 'tree' ]
     ]);
@@ -161,21 +69,6 @@ module.exports = function (app) {
             ]
         ]
     ]);
-
-    /*app.get('/quests', function (req, res) {
-        res.send(controllers.quests.list());
-    });*/
-    /*app.param('questId', function (req, res, next, name) {
-        var regex = new RegExp(/^[0-9]$/);
-        if (regex.test(name)) {
-            next();
-        } else {
-            next('route');
-        }
-    });
-    app.post('/quests', function (req, res) {
-        res.send(controllers.quests.startMission(req.params.questId));
-    });*/
 
     // Frontend
     /*jslint unparam: true */

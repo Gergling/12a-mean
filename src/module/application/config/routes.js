dc4345d36464acc43d7a65ae7d197c2e0611e39e
gergling <gregmichaeldavies@gmail.com>
2015-05-24 14:34:57 +0100

    Extra exceptions and linting.

--- src/module/application/config/routes.js
+++ src/module/application/config/routes.js
@@ -7,25 +7,42 @@ module.exports = function (app) {
                 var method = route[0],
                     url = route[1],
                     functionName = route[2],
-                    params = route[3];
+                    params = route[3],
+                    fnc = controller[functionName];
 
                 if (params) {
-                    params.forEach(function (param) {
-                        var paramName = param[0],
-                            regex = param[1];
+                    if (params.constructor === [ ].constructor) {
+                        params.forEach(function (param) {
+                            var paramName = param[0],
+                                regex = param[1];
 
-                        /*jslint unparam: true */
-                        app.param(paramName, function (req, res, next, name) {
-                            if (regex.test(name)) {
-                                next();
-                            } else {
-                                next('route');
-                            }
+                            /*jslint unparam: true */
+                            app.param(paramName, function (req, res, next, name) {
+                                if (regex.test(name)) {
+                                    next();
+                                } else {
+                                    next('route');
+                                }
+                            });
+                            /*jslint unparam: false */
                         });
-                        /*jslint unparam: false */
-                    });
+                    } else {
+                        throw new Error("Third route element should be an array. '" + params.constructor + "' given.");
+                    }
                 }
-                app[method](url, controller[functionName]);
+                if (app[method]) {
+                    if (fnc) {
+                        if (typeof fnc === 'function') {
+                            app[method](url, fnc);
+                        } else {
+                            throw new Error("Routing function parameter '" + controllerPath + "." + functionName + "' should be a function. " + ( typeof fnc ) + " found.");
+                        }
+                    } else {
+                        throw new Error("No routing function in '" + controllerPath + "' named '" + functionName + "'.");
+                    }
+                } else {
+                    throw new Error("No method function '" + method + "'.");
+                }
             });
         };
 

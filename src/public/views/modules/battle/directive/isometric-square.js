68a07d2e2151bcfd3d70a3c56ec0620e97119aeb
gergling <gregmichaeldavies@gmail.com>
2015-05-31 01:00:37 +0100

    Removed ngModules from redundant isometric battle directives.

--- src/public/views/modules/battle/directive/isometric-square.js
+++ src/public/views/modules/battle/directive/isometric-square.js
@@ -1,21 +1,20 @@
-ngModules.get("battle").component(function (ngm, mod) {
+angular.module("battle").directive("battleIsometricSquare", function () {
+
     "use strict";
 
-    ngm.directive("battleIsometricSquare", function () {
-        return {
-            restrict: "A",
-            scope: {x: "@", y: "@", width: "@", height: "@"},
-            templateUrl: 'modules/' + mod.getPartialUrl("isometric-square"),
-            controller: [
+    return {
+        restrict: "A",
+        scope: {x: "@", y: "@", width: "@", height: "@"},
+        templateUrl: "modules/battle/partial/isometric-square.html",
+        controller: [
 
-                "$scope",
+            "$scope",
 
-                function ($scope) {
-                    $scope.isometricX = $scope.x * $scope.width;
-                    $scope.isometricY = $scope.y * $scope.height;
-                    $scope.character = 3; // An id. This will be used to generate a directive for displaying character models.
-                }
-            ]
-        };
-    });
+            function ($scope) {
+                $scope.isometricX = $scope.x * $scope.width;
+                $scope.isometricY = $scope.y * $scope.height;
+                $scope.character = 3; // An id. This will be used to generate a directive for displaying character models.
+            }
+        ]
+    };
 });

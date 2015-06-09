47b12e793f30d29b452f0e33c0c3a8a323079348
gergling <gregmichaeldavies@gmail.com>
2015-06-09 23:24:04 +0100

    Removed superfluous code.

--- src/public/views/modules/application/service/primary-navigation.js
+++ src/public/views/modules/application/service/primary-navigation.js
@@ -13,33 +13,18 @@ angular.module("application").service("application.service.primary-navigation", 
                 name: name,
                 label: label,
                 sref: sref || 'container.' + name
-                //sref: name
             };
             scope.list.push(item);
         };
         this.list = [];
 
-        //setNavItem("mess", "Mess");
         setNavItem("quests", "Bridge");
-        //setNavItem("cargo-bay", "Cargo Bay");
-        //setNavItem("skills", "Skills", "container.skills({skill: ''})");
         setNavItem("skills", "Skills");
 
         $rootScope.$on("$stateChangeSuccess", angular.bind(this, function () {
-            console.log($state.current.name, $stateParams);
             this.list.forEach(function (item) {
                 item.active = item.sref === $state.current.name;
             });
         }));
-        /*this.setActive = function (name) {
-            angular.forEach(scope.list, function (item) {
-                if (item.name === name) {
-                    item.active = true;
-                    scope.active = item;
-                } else {
-                    item.active = false;
-                }
-            });
-        };*/
     }
 ]);

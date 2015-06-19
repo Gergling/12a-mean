2a6214f9500e78291b5cb6072a9e6e0982091676
gergling <gregmichaeldavies@gmail.com>
2015-06-19 09:10:21 +0100

    Indentation improvement.

--- src/module/user/controller.js
+++ src/module/user/controller.js
@@ -23,32 +23,33 @@ module.exports = (function () {
     });
 
     passport.use('login', new LocalStrategy({
-        passReqToCallback : true
-    },
-    function(req, username, password, done) { 
-        // Todo: Returns a 404, but runs correctly. Investigate.
-        // check in mongo if a user with username exists or not
-        User.findOne({ 'username' :  username }, 
-            function(err, user) {
-                // In case of any error, return using the done method
-                if (err) {return done(err); }
-                // Username does not exist, log error & redirect back
-                if (!user) {
-                    console.log('User Not Found with username '+username);
-                    return done(null, false, 
-                    req.flash('message', 'User Not found.'));                 
+            passReqToCallback : true
+        },
+        function(req, username, password, done) { 
+            // Todo: Returns a 404, but runs correctly. Investigate.
+            // check in mongo if a user with username exists or not
+            User.findOne({ 'username' :  username }, 
+                function(err, user) {
+                    // In case of any error, return using the done method
+                    if (err) {return done(err); }
+                    // Username does not exist, log error & redirect back
+                    if (!user) {
+                        console.log('User Not Found with username '+username);
+                        return done(null, false, 
+                        req.flash('message', 'User Not found.'));                 
+                    }
+                    // User exists but wrong password, log the error 
+                    if (!isValidPassword(user, password)) {
+                        console.log('Invalid Password');
+                        return done(null, false, req.flash('message', 'Invalid Password'));
+                    }
+                    // User and password both match, return user from 
+                    // done method which will be treated like success
+                    return done(null, user);
                 }
-                // User exists but wrong password, log the error 
-                if (!isValidPassword(user, password)) {
-                    console.log('Invalid Password');
-                    return done(null, false, req.flash('message', 'Invalid Password'));
-                }
-                // User and password both match, return user from 
-                // done method which will be treated like success
-                return done(null, user);
-            }
-        );
-    }));
+            );
+        }
+    ));
 
     passport.use('signup', new LocalStrategy({
         passReqToCallback : true
